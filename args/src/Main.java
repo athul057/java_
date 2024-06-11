f5f036b192f0f7f444059a7591243b4249cb7b0c
athul <athuli057@gmail.com>
2024-06-12 00:05:15 +0530

    public static void main(String[] args project

---
+++ args/src/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Arrays;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        //Through this we can pass the arguments through command line
+
+
+        System.out.println(Arrays.toString(args));
+
+    }
+}
