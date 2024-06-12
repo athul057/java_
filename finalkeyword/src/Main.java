baf718ea32d05d00f819ecce83bb894eb157e0df
athul <athuli057@gmail.com>
2024-06-12 23:43:59 +0530

    enum and final

---
+++ finalkeyword/src/Main.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static final String NAME="ali";
+
+
+    enum Gender{
+            MALE,
+        FEMALE
+    }
+    enum TShirtSize{
+        S,M,L,XL
+    }
+    public static void main(String[] args) {
+System.out.println(NAME);
+Point p=new Point();
+p.x=10;
+p.y=20;
+        System.out.println(Math.PI);
+        System.out.println(Gender.MALE);
+        Main.Gender athul=Gender.MALE;
+        System.out.println(athul);
+        System.out.println(Month.february);
+        var myDistrict=Kerala.KANNUR;
+        System.out.println(myDistrict);
+
+    }
+}
