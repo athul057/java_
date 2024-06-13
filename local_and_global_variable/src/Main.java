9404dcc6126750b5e5cebc552c324f983b5d9c9e
athul <athuli057@gmail.com>
2024-06-13 11:28:20 +0530

    local and global variables

---
+++ local_and_global_variable/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+
+
+    //THIS IS GLOBAL VARIBALE.....
+    private static final String NAME="ATHUL";
+    private static final double PI=3.14;
+
+    public static void main(String[] args) {
+
+        //THIS ARE LOCAL VARIABLE....
+
+        var NAME="AKARSH";
+        var country="INDIA";
+
+        //HERE IT WILL FIRST CHECK THE LOCAL VARIABLE...IT HAS HIGHER PREFERENCE.
+        System.out.println(NAME);
+
+
+    }
+}
