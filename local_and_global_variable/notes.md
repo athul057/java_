4d9a14366ff1784e9c4f0ee051d2d34c5e7c3658
athul <athuli057@gmail.com>
2024-06-13 12:48:08 +0530

    local and global variables

---
+++ local_and_global_variable/notes.md
@@ -0,0 +1,15 @@
+## Parse Int and Parse Double
+
+To convert string to Integer as well as String to Double 
+
+
+we can use `parseInt() and parseDouble()` methods...
+
+this will be in `Integer` and `Double` class in java
+
+
+`
+String a="1";
+double b=Double.parseDouble(a);
+int c=Integer.parseInt(a);
+`
--- local_and_global_variable/src/Main.java
+++ local_and_global_variable/src/Main.java
@@ -14,6 +14,14 @@ public class Main {
         var NAME="AKARSH";
         var country="INDIA";
 
+
+        //Converting an String to double as well as integer....
+        String a="1";
+        double b=Double.parseDouble(a);
+        int c=Integer.parseInt(a);
+        System.out.println(b);
+        System.out.println(c);
+
         //HERE IT WILL FIRST CHECK THE LOCAL VARIABLE...IT HAS HIGHER PREFERENCE.
         System.out.println(NAME);
 
