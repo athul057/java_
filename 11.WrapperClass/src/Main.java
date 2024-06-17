f70c9c0319c1895e810541c6923a6ff8bb5030f4
athul <athuli057@gmail.com>
2024-06-17 18:47:34 +0530

    wrapper class

---
+++ 11.WrapperClass/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String args[]){
+        int a=20;
+        Integer b=45;
+        Double c=50.55;
+        var val="57";
+        int k=Integer.parseInt(val);
+        Double h=Double.parseDouble(val);
+
+        System.out.println(Integer.max(30,55));
+
+        String myVal=Integer.toString(69);
+        System.out.println(myVal);
+
+
+    }
+}
