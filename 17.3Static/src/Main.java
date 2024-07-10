e5181da548d6585a077fd09841f4340dcf1d54e3
athul <athuli057@gmail.com>
2024-07-10 07:40:29 +0530

    static

---
+++ 17.3Static/src/Main.java
@@ -0,0 +1,12 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Person Ali=new Person("ali",999,Gender.MALE);
+        Person David=new Person("David",912,Gender.MALE);
+        Person Risha=new Person("Risha",888,Gender.FEMALE);
+        System.out.println(Person.count);
+
+        }
+    }
