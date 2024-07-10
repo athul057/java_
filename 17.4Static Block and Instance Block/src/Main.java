766efa4d9ec62d1533468b749d4e217a764ac207
athul <athuli057@gmail.com>
2024-07-10 17:10:44 +0530

    static and instance block

--- 17.4Static Block and Instance Block/src/Main.java
+++ 17.4Static Block and Instance Block/src/Main.java
@@ -1,9 +1,13 @@
 //156
+import static java.lang.Math.max;
 public class Main {
     public static void main(String[] args) {
         Person athul=new Person();
         System.out.println();
         Person akhil=new Person();
         System.out.println(Person.count);
+        System.out.println(Math.max(2,300));
+
+        System.out.println(max(5,800));
     }
 }
